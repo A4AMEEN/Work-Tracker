e6ccadc3dc98843c5e6f69f4873cb4cebc07df0a
Ameen Aneez <ameen.aneez@aromacontracting.com>
2026-08-10 09:02:39 +0400

    Push to new

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
- apiUrl: 'https://tracker-backend-black-zeta.vercel.app/api'
+ apiUrl: 'https://work-tracker-ten-omega.vercel.app/api'
    //apiUrl: 'http://localhost:3000/api'
 };
